44b88f50397987a1cee14871ba3f03782f9d55a8
LuisCortesTec <a01631163@itesm.mx>
2020-02-26 02:42:34 +0000

    pratica 1

---
+++ xv6-public/apagar.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+void
+cat(int fd)
+{
+  int n;
+
+  while((n = read(fd, buf, sizeof(buf))) > 0) {
+    if (write(1, buf, n) != n) {
+      printf(1, "cat: write error\n");
+      exit();
+    }
+  }
+  if(n < 0){
+    printf(1, "cat: read error\n");
+    exit();
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+    printf(1, "Hasta la vista....\n");
+    shutdown();
+    exit();
+}
